35a8a835d3446975a7bf20a1eb9a101a1e1f55ad
OGReaper <102786953+OGReaperx@users.noreply.github.com>
2025-04-02 15:27:42 +1300

    added support for boxzones

--- shared/config.lua
+++ shared/config.lua
@@ -9,5 +9,12 @@ Config.RequiredPhoneItem = {  -- item required to unlock the phone add more
 Config.PhoneMetadataKey = 'phoneNumber' -- 'phoneNumber' for 'lb-phone' | 'imei' for 'yseries' | metadata key to store the phone number
 
 Config.Computers = {
-    {coords = vec3(68.85, -387.95, 53.2), radius = 0.4}, -- Example coordinates where the computer is placed
+    {
+        coords = vec3(68.85, -387.95, 53.2), 
+        radius = 0.4
+    }, -- This will trigger spherezone
+    {
+        -- coords = vec3(200.0, -300.0, 54.0),
+        -- size = vec3(2.0, 2.0, 2.0) -- will trigger box zone
+    }
 }

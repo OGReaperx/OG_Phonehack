d2d5756003f877ec18a65421a5b58e917e9d8f0b
OGReaper <102786953+OGReaperx@users.noreply.github.com>
2025-04-03 20:59:03 +1300

    New config options

--- shared/config.lua
+++ shared/config.lua
@@ -6,6 +6,11 @@ Config.RequiredPhoneItem = {  -- item required to unlock the phone add more
     'phone',
 }
 
+Config.Webhook = '' -- webhook to send logs to
+Config.WebhookColor = "#00ff99" -- webhook color (hex format) - default is #00ff99
+Config.WebhookColor_Corrupt = "#ff0000" -- webhook color (hex format) - default is #ff0000
+Config.WebhookImage = "https://cdn-icons-png.flaticon.com/128/597/597177.png" -- webhook image using flaticon.com
+
 Config.PhoneMetadataKey = 'phoneNumber' -- 'phoneNumber' for 'lb-phone' | 'imei' for 'yseries' | metadata key to store the phone number
 
 Config.Computers = {
